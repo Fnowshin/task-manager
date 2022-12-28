45a034e1d53a7edca997982f9ee7850226ff8508
Fnowshin <fnowshin5@gmail.com>
2022-12-29 04:42:34 +0600

    homepage

--- src/Pages/AddTask/TaskForm.js
+++ src/Pages/AddTask/TaskForm.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TaskForm = (props) => {
+const TaskForm = () => {
+
+    const [task, setTask] = useState('');
+    const [updated, setUpdated] = useState('');
+
+    const handleAddedTask = event =>{
+        event.preventDefault();
+        console.log(task);
+        
+    }
+
+    const handleChange = event => {
+        setTask(event.target.value)
+    }
+   
+    const handleKeyDown = event =>{
+        if(event.key === 'Enter'){
+            setUpdated(updated);
+            
+            
+        }
+    }
+
     return (
         <div className='my-20'>
-            <form novalidate="" action="" className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
+            <form onSubmit={handleAddedTask} className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
             <fieldset className="p-4 rounded-sm p-6 rounded-md shadow-xl dark:bg-green-900">
 
 			<div className="col-span-full sm:col-span-3">
-					<label for="addtask" className="mx-auto font-bold">Add Task </label>
-					<input id="addtask" type="text" placeholder="Add Task" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-green-400 dark:border-green-700 dark:text-gray-900" />
+					<label className="mx-auto font-bold">Add Task </label>
+					<input id="addtask" type="text" name='task' onChange={handleChange} 
+                    onKeyDown={handleKeyDown} placeholder="Add Task" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-green-400 dark:border-green-700 dark:text-gray-900" />
 				</div>
                 <div>
-                <button type="button" className="px-8 py-3 mt-6 font-semibold rounded-full bg-green-400 text-gray-800">Add </button>
+                <button type="submit" className="px-8 py-3 mt-6 font-semibold rounded-full bg-green-400 text-gray-800">Add </button>
                 </div>
 		</fieldset>
         </form>
